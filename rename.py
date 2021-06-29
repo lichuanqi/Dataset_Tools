95962ea3c484cb5463cd75f384ec96b4536e27c9
“lichuanqi” <lc@dlc618.com>
2021-06-29 14:04:48 +0800

    Fix: file rename

--- rename.py
+++ rename.py
@@ -7,13 +7,40 @@
 
 
 import os,sys
- 
-cur_path = 'D:/camvid/camvid/labels' # 你的数据集路径
- 
-labels = os.listdir(cur_path)
- 
-for label in labels:
-    old_label = str(label)
-    new_label = label.replace('_P.png','.png')
-    print(old_label, new_label)
-    os.rename(os.path.join(cur_path,old_label),os.path.join(cur_path,new_label))
+
+def rename_label():
+    # 你的数据集路径
+    cur_path = '/media/lcq/Data/modle_and_code/DataSet/RailSample/JPEGImages'
+
+    labels = os.listdir(cur_path)
+    
+    for label in labels:
+        old_label = str(label)
+        new_label = label.replace('_P.png','.png')
+        print(old_label, new_label)
+        os.rename(os.path.join(cur_path,old_label),os.path.join(cur_path,new_label))
+
+    return 'Finished'
+
+
+def rename_file():
+    # 你的数据集路径
+    path = '/media/lcq/Data/modle_and_code/DataSet/RailSample/JPEGImages'
+    files = os.listdir(path)
+    
+    print('============= Start =============')
+    print('File Numners: %s'%(len(files)))
+
+    for file in files:
+        old_label = str(file)
+        new_label = file.split('；')[0] + '.' + file.split('.')[1]
+        print(old_label + ' > ' + new_label)
+        os.rename(os.path.join(path,old_label),os.path.join(path,new_label))
+
+    print('============== End ==============')
+
+    return 'Finshed'
+
+
+if __name__=='__main__':
+    rename_file()
