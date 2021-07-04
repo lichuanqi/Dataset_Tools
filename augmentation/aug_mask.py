481c11fb4590128d3f18a77b735842918af51574
“lichuanqi” <lc@dlc618.com>
2021-07-04 22:26:33 +0800

    修改：数据扩增方法及相关参数

--- augmentation/aug_mask.py
+++ augmentation/aug_mask.py
@@ -16,29 +16,31 @@ from albumentations import (
     Compose,
     OneOf,
 
-    Resize,                    # 拉伸图片修改尺寸
-    PadIfNeeded,
     HorizontalFlip,            # 随机水平翻转
     VerticalFlip,              # 随机垂直翻转
-    CenterCrop,
-    Crop,
-    Transpose,
-    RandomRotate90,            # 随机90度旋转
-    ElasticTransform,
-    GridDistortion,
-    OpticalDistortion,
-    RandomSizedCrop,           # 随机尺寸裁剪并缩放回原始大小
-    RandomResizedCrop,         # 随机尺寸裁剪并缩放指定大小
     CLAHE,                     # 对比度受限状况下的自适应直方图均衡化算法
-    Normalize,                 # 标准化
     RandomBrightnessContrast,  # 随机选择图片的对比度和亮度
     RandomGamma,               # Gamma变换
     Blur,                      # 滤波
     MotionBlur,                # 运动滤波
     MedianBlur,                # 中值滤波
-    ShiftScaleRotate,          # 平移/缩放/旋转三合一
+    CoarseDropout,             # 随机填充黑色
     Downscale,                 # 降低分辨率
     Rotate,                    # 旋转
+    ShiftScaleRotate,          # 平移/缩放/旋转三合一
+
+    #  未使用
+    Resize,                    # 拉伸图片修改尺寸
+    PadIfNeeded,
+    CenterCrop,
+    Crop,
+    Transpose,
+    ElasticTransform,
+    GridDistortion,
+    OpticalDistortion,
+    RandomSizedCrop,           # 随机尺寸裁剪并缩放回原始大小
+    RandomResizedCrop,         # 随机尺寸裁剪并缩放指定大小
+    Normalize,                 # 标准化
     )
 
 
@@ -67,9 +69,11 @@ def mask_aug():
             MotionBlur(blur_limit=5, p=1),
             MedianBlur(blur_limit=5, p=1),],p=1),
         
-        RandomSizedCrop(min_max_height=(700, 1000), height=1080, width=1920, p=1),
+        CoarseDropout(p=1.0, max_holes=40, max_height=40, max_width=40, min_holes=10, min_height=10, min_width=10),
         Downscale(p=1.0, scale_min=0.25, scale_max=0.25, interpolation=0),
-        Rotate(p=1.0, limit=(-40, 40), interpolation=0, border_mode=0, value=(0, 0, 0), mask_value=None)
+        Rotate(p=1.0, limit=(-40, 40), interpolation=0, border_mode=0, value=(0, 0, 0), mask_value=None),
+        ShiftScaleRotate(p=1.0, shift_limit=(-0.1, 0.1), scale_limit=(-0.25, 0.25), rotate_limit=(-25, 25)),
+        ShiftScaleRotate(p=1.0, shift_limit=(-0.25, 0.25), scale_limit=(-0.1, 0.1), rotate_limit=(-25, 25))
         ])
 
     return aug
@@ -77,18 +81,18 @@ def mask_aug():
 def main():
 
     # 测试
-    # train_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/jpgs/')  # 输入 img 地址
-    # mask_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/masks/')  # 输入 mask 地址
-    # augtrain_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/aug_jpgs/')  # 输入增强img存放地址
-    # augmask_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/aug_masks/')  # 输入增强mask存放地址
+    train_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/jpgs/')  # 输入 img 地址
+    mask_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/masks/')  # 输入 mask 地址
+    augtrain_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/aug_jpgs/')  # 输入增强img存放地址
+    augmask_path = ('/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/aug_masks/')  # 输入增强mask存放地址
     
     # RailGuard
-    train_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/jpgs/')  # 输入 img 地址
-    mask_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/masks/')  # 输入 mask 地址
-    augtrain_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/aug_jpgs/')  # 输入增强img存放地址
-    augmask_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/aug_masks/')  # 输入增强mask存放地址
+    # train_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/jpgs/')  # 输入 img 地址
+    # mask_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/masks/')  # 输入 mask 地址
+    # augtrain_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/aug_jpgs/')  # 输入增强img存放地址
+    # augmask_path = ('/media/lcq/Data/modle_and_code/DataSet/RailGuard200/aug_masks/')  # 输入增强mask存放地址
 
-    num = 6  # 输入增强图像增强的张数。
+    num = 8  # 输入增强图像增强的张数。
 
     train_img, masks = data_num(train_path, mask_path)
     # print(train_img)
@@ -99,13 +103,13 @@ def main():
 
     for data in range(len(train_img)):
 
-        # 绝对路径
-        jpg_path_temp = os.path.join(train_path, train_img[data])
-        mask_path_temp = os.path.join(mask_path, masks[data])
-
         # 文件名
         image_name = train_img[data].split('.')[0]
         # print(image_name)
+
+        # 绝对路径
+        jpg_path_temp = os.path.join(train_path, image_name+'.jpg')
+        mask_path_temp = os.path.join(mask_path, image_name+'.png')
 
         # 分别读取
         image = cv2.imread(jpg_path_temp)
--- visualization/viz_dir.py
+++ visualization/viz_dir.py
@@ -1,6 +1,6 @@
 #==============================================
 # 语义分割数据集的可视化查看
-# 文件夹的读取待完成
+# 查看文件夹内所以数据
 # lichuan
 # lc@dlc618.com
 # 2021.7.2
@@ -8,4 +8,62 @@
 
 import matplotlib.pyplot as plt
 import cv2
+
+import os,glob
+
+def get_filename(jpg_dir, masks_dir):
+
+    jpgs = os.listdir(jpg_dir)
+    masks = os.listdir(masks_dir)
+
+    return jpgs, masks
+
+
+def show_all(jpg_dir, masks_dir):
+
+    jpgs, masks = get_filename(jpg_dir, masks_dir)
+    current, key = 0, ord('a')
+
+    while key > ord(' '):
+        
+        name_temp = jpgs[current].split('.')[0]
+        jpg_temp = os.path.join(jpg_dir,name_temp+'.jpg')
+        mask_temp = os.path.join(masks_dir,name_temp+'.png')
+
+        jpg = cv2.imread(jpg_temp)
+        mask = cv2.imread(mask_temp, 0)
+
+        contours, hie = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        img_fill = jpg.copy()
+        for ci in range(len(contours)):
+            # 轮廓
+            # cv2.drawContours(img, contours[i], -1, (0, 0, 255), 3)
+            # 内部填充
+            cv2.fillConvexPoly(img_fill, contours[ci], (0, 0, 255))
+    
+        # 按权重叠加
+        img_add = cv2.addWeighted(jpg, 0.6, img_fill, 0.4, 0)
+        text = '{}/{}: {}'.format(current+1, len(jpgs), name_temp)
+        cv2.putText(img_add, text, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 2)
+        
+        cv2.imshow('Windows', img_add)
+
+        #use 'a' and 'd' to scroll through frames
+        key = cv2.waitKey(-1) 
+        current = current - 1 if key == ord('a') else current + 1
+        current = (current + len(jpgs)) % len(jpgs) #wrap around
+    
+    return 0
+
+if __name__=='__main__':
+
+    # 测试路径
+    jpg_dir = '/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/aug_jpgs'
+    masks_dir = '/media/lcq/Data/modle_and_code/DataSet/Segmentation_Dataset_Tools/dataset/aug_masks'
+    
+    # RailGuard
+    # jpg_dir   = '/media/lcq/Data/modle_and_code/DataSet/RailGuard200/jpgs'
+    # masks_dir = '/media/lcq/Data/modle_and_code/DataSet/RailGuard200/masks'
+ 
+    show_all(jpg_dir, masks_dir)
 
