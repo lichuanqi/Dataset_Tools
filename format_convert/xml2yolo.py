76008abee03492c41f7872d471f540f8f9afe752
lichuanqi <lc@dlc618.com>
2022-04-01 11:12:33 +0800

    修改：根据目标类型确定目标索引，当不存在跳过

--- format_convert/xml2yolo.py
+++ format_convert/xml2yolo.py
@@ -5,6 +5,7 @@
 # 2021.7.20
 #==============================================
 
+import time
 import cv2
 import os
 import xml.etree.ElementTree as ET
@@ -49,28 +50,37 @@ def xml_txt(txt_path,
 			line = ''
 			for item in root.findall('object'):
 				# 提取label,并获取索引
-				label = item.find('name').text
-				label = labels.index(label)
-				# 提取信息labels, x, y, w, h 
-				# 多框转化
-				for box in item.findall('bndbox'):
-					xmin = float(box.find('xmin').text)
-					ymin = float(box.find('ymin').text)
-					xmax = float(box.find('xmax').text)
-					ymax = float(box.find('ymax').text)
+				label_name = item.find('name').text
 
-					# x, y, w, h 归一化
-					center_x = ((xmin + xmax) / 2) * dw
-					center_y = ((ymin + ymax) / 2) * dh
-					bbox_width = (xmax-xmin) * dw
-					bbox_height = (ymax-ymin) * dh
-					
-					print('--From:',xmin,ymin,xmax,ymax)
-					print('--To  :',center_x,center_y,bbox_width,bbox_height)
-					
-					# 传入信息，txt是字符串形式
-					line += '{} {} {} {} {}'.format(label,center_x,center_y,bbox_width,bbox_height) + '/n'              
-			
+				# 根据目标类型确定目标索引，当不存在跳过
+				try:
+					label = labels.index(label_name)
+				
+					# 提取信息labels, x, y, w, h 
+					# 多框转化
+					for box in item.findall('bndbox'):
+						xmin = float(box.find('xmin').text)
+						ymin = float(box.find('ymin').text)
+						xmax = float(box.find('xmax').text)
+						ymax = float(box.find('ymax').text)
+
+						# x, y, w, h 归一化
+						center_x = ((xmin + xmax) / 2) * dw
+						center_y = ((ymin + ymax) / 2) * dh
+						bbox_width = (xmax-xmin) * dw
+						bbox_height = (ymax-ymin) * dh
+						
+						print('  From:',label_name,xmin,ymin,xmax,ymax)
+						print('  To  :',label,center_x,center_y,bbox_width,bbox_height)
+						
+						# 传入信息，txt是字符串形式 - Win
+						line += '{} {} {} {} {}'.format(label,center_x,center_y,bbox_width,bbox_height) + '\n'              
+						# 传入信息，txt是字符串形式 - Linux
+						# line += '{} {} {} {} {}'.format(label,center_x,center_y,bbox_width,bbox_height) + '/n'              
+				
+				except ValueError:
+						print('  From: {} {} {} {} {} {}  To: Skiped',label_name,xmin,ymin,xmax,ymax)
+
 			# 将txt信息写入文件
 			with open(ftxt_path, 'w') as f_txt:
 				f_txt.write(line)
@@ -83,9 +93,18 @@ if __name__ == '__main__':
 	# 存放xml的文件目录
 	xml_path = 'D:/Code/DATASET/RailSample/xmls'
 	# yolo存放生成txt的文件目录
-	txt_path = 'D:/Code/DATASET/RailSample/txt'
+	txt_path = 'D:/Code/DATASET/RailSample/txts_2'
+
+	# 测试
+	# image_path = 'dataset/jpgs'
+	# xml_path = 'dataset/xml'
+	# txt_path = 'dataset/txt'
+	# labels = ['person', 'train']
 
 	# 标签
+	
+	labels_railsample_2 = ['person', 'train']
+	
 	labels_railsample = ['person', 'bicycle', 'car', 'motorbike', 'tricycle', 'train', 'truck',
 				   'forklift', 'bus', 'bag', 'dog', 'box', 'stone', 'light']
 
@@ -96,4 +115,7 @@ if __name__ == '__main__':
 					'pinD_miss', 'nut_normal', 'pinA_latent',
 					'pinB_latent', 'pinC_miss', 'pinC_latent' ]
 
-	xml_txt(txt_path, image_path, xml_path, labels_railsample)
+	xml_txt(txt_path, image_path, xml_path, labels_railsample_2)
+
+	print('Over')
+	time.sleep(50)
